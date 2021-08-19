97c25cb8f49f609f9116c103051bd8324e0b8557
张锦华 <colin.zhang@zsgcm.com>
2021-08-19 21:26:12 +0800

    add member picture sql

---
+++ towako-server/src/main/resources/db/migration/V0026__create_medical_member_picture_tables.sql
@@ -0,0 +1,14 @@
+-- 病历表
+alter table ar_medical_records
+    add member_id bigint NULL COMMENT '会员Id' after hospital_id;
+
+-- 用户自传病历图片
+CREATE TABLE `ar_medical_member_pictures` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `medical_record_id` bigint NULL COMMENT '病历Id',
+  `member_id` bigint NULL COMMENT '用户Id',
+  `url` varchar(256) NULL COMMENT '图片地址',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户自传病历图片';
